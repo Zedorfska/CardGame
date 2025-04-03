58fbf840795cf815657793dc6ba273cc882414a5
Zedorfska <138994095+Zedorfska@users.noreply.github.com>
2025-04-03 02:53:09 +0200

    Update main.gd

--- main.gd
+++ main.gd
@@ -3,11 +3,22 @@ extends Node
 var Table1A
 
 var TestCard = preload("res://TestCard.tscn")
+var ListOfCards = [
+	TestCard
+]
+var Deck = []
 
 func _ready():
 	Table1A = $CanvasLayer/Control/Root/Middle/Player1Table/Player1Card1
 	print("ready")
-	print(TestCard)
+	refill_deck()
+
+func refill_deck():
+	for Card in range(ListOfCards.size()):
+		print(Card)
+		Deck.append(ListOfCards[Card])
+	print(Deck)
+
 
 # temp
 func _on_button_pressed() -> void:
