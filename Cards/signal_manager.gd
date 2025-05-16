52ec9bb2dd66a10e6cfb524fea062d7bc949097a
Zedorfska <138994095+Zedorfska@users.noreply.github.com>
2025-05-16 12:34:51 +0100

    WHOOOPS unnecessary

--- Cards/signal_manager.gd
+++ Cards/signal_manager.gd
@@ -1,10 +1,6 @@
 extends Node
 
 signal amount_of_cards_on_table_changed_signal
-signal card_played_signal
 
 func amount_of_cards_on_table_changed():
 	amount_of_cards_on_table_changed_signal.emit()
-
-func card_played():
-	card_played_signal.emit()
